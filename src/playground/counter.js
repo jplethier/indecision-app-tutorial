64027355dc0ee7fba21497f82b16260157ffed86
João Paulo Lethier <jplethier@gmail.com>
2017-11-15 14:51:24 -0200

    refactoring counter.js to use react components and react state;

--- src/playground/counter.js
+++ src/playground/counter.js
@@ -1,33 +1,36 @@
-let count = 0;
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    }
+    this.handleAddOne = this.handleAddOne.bind(this)
+    this.handleMinusOne = this.handleMinusOne.bind(this)
+    this.handleReset = this.handleReset.bind(this)
+  }
 
-const addOne = () => {
-  count++;
-  renderCounterApp();
+  handleAddOne() {
+    this.setState({ count: this.state.count + 1 })
+  }
+
+  handleMinusOne() {
+    this.setState({ count: this.state.count - 1 })
+  }
+
+  handleReset() {
+    this.setState({ count: 0 })
+  }
+
+  render() {
+    const { count } = this.state;
+
+    return (
+      <div>
+        <h1>Count: {count}</h1>
+        <button onClick={this.handleAddOne}>+1</button>
+        <button onClick={this.handleMinusOne}>-1</button>
+        <button onClick={this.handleReset}>Reset</button>
+      </div>
+    )
+  }
 }
-
-const minusOne = () => {
-  count--;
-  renderCounterApp();
-}
-
-const reset = () => {
-  count = 0;
-  renderCounterApp();
-}
-
-const appRoot = document.getElementById('app')
-
-const renderCounterApp = () => {
-  const templateTwo = (
-    <div>
-      <h1>Count: {count}</h1>
-      <button onClick={addOne}>+1</button>
-      <button onClick={minusOne}>-1</button>
-      <button onClick={reset}>Reset</button>
-    </div>
-  );
-
-  ReactDOM.render(templateTwo, appRoot);
-}
-
-renderCounterApp();
